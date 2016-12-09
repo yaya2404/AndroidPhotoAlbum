4992f08885c468bb2331ce790e31670d8389c58b
Matthew Ya <matthew_ya@yahoo.com>
2016-12-09 15:54:44 -0500

    test

--- PhotoAlbum/app/src/main/java/group26/photoalbum/PhotoAlbumHomeScreen.java
+++ PhotoAlbum/app/src/main/java/group26/photoalbum/PhotoAlbumHomeScreen.java
@@ -15,17 +15,7 @@ public class PhotoAlbumHomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.album_homescreen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     /*
